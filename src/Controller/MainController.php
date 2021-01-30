5a7a1f9d0d46b84848c7b67611cea17df7728e6a
Jacob Proulx <jacobproulx@gmail.com>
2021-01-30 13:25:11 -0500

    Remise à 0 de PHY144

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,33 +2,19 @@
 
 namespace App\Controller;
 
-use App\Classes\Vecteur;
-use App\Service\sVecteur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'main')]
-    public function index(sVecteur $sVecteur): Response
+    #[Route('/', name: 'main')]
+    public function index(): Response
     {
-        $v1 = new Vecteur(105, 25, Vecteur::CADRAN_II_Y);
-        $v2 = new Vecteur(33, 20, Vecteur::CADRAN_II_X);
-        $v3 = new Vecteur(180, 75, Vecteur::CADRAN_I_X);
-
-        $sVecteur
-            ->addVecteur($v1)
-            ->addVecteur($v2)
-            ->addVecteur($v3)
-        ;
-
-        dump($v1);
-        dump($v2);
-        dump($v3);
+        dump('Hello main controller');
 
         return $this->render('main/index.html.twig', [
-            'sVecteur' => $sVecteur,
+
         ]);
     }
 }
